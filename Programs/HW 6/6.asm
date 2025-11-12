632c36f58e2d426ee49d03e5c6558a8ac61c21ae
Johang727 <johanghernandez123@gmail.com>
2025-11-11 23:59:35 -0600

    input logic works

--- Programs/HW 6/6.asm
+++ Programs/HW 6/6.asm
@@ -1,22 +1,53 @@
-		; allocate a block of 7 bytes for the characters
-		; allocate a block of 2 bytes for the find input
-		; allocate a block of 2 bytes for the replacement input
+; variables
 
-		; prompt user for input
+; registers:
+	; R1: pointer storage
+	; R2: counter (char limit)
+	; R3: temporary character storage
+	; R4: counter (replacement char limit)
+		; this has to be equal to find to not overflow
 
-		; get input
-			; set pointer to start of string
-			; while not new line or # && counter != 0
-				; get character and store it in the INPUT variable
-				; have a pointer that increments or something to do this
+; memory:
+	; locations: 
+		; START: Start of the program to loop back to at the end
+
+		; RD_LP: Read Loop - Start of the Reading Loop
+		; SKP_EX: Skip Exit - Skip checking for # if it's not the first char.
+		; RD_EX: Read Exit - Allows for continuing on newline, prints out a newline after
+
+		; FND: Start of FIND input reading logic
+		; FD_LP: Find Loop - Start of the Reading Loop for what to find
+
+
+		; HT: Halt - In case I want to do something before halting.
+
+	; variables
+		; INPUT: A block of 8 memory locations to hold the string to be modified
+		; FIND: A blk of 3 mem locs to hold what needs to be found in INPUT
+		; REPLACE: A blk of 3 mem locs to hold what needs to be replaced 
+			; Each of these have an extra location so that PUTS always finds an exit char x0000
+
+		; PRMTIN: Prompt In: Custom input prompt (for INPUT)
+		; PRMTFD: Prompt Find: Custom input prompt (for FIND)
+		; PRMTRP: Prompt Replace: Custom input prompt (for REPLACE)
+		
+		; ALTIN: Alert In: Tell the user that nothing will be done
+		;			because their input was empty.
+		; ALTFD: Alert Find: Tell the user that nothing will be searched for if
+		; 		query is empty
+
+		; NWLN: Holds a newline character
+
+		; COUNT7: Holds the number 7 to init a counter
+		; COUNT2: Holds the number 2 to init a counter
 
 
 	.ORIG	x3000
 
-	; Reading Logic
+; Reading Logic
 
 	; print prompt
-	LEA	R0,	PRMTIN
+START	LEA	R0,	PRMTIN
 	TRAP	x22 ; PUTS
 
 	LD	R2,	COUNT7 ; load the number 7 into R2 for counting
@@ -60,44 +91,118 @@ SKP_EX	ADD	R2,	R2,	#7
 	; print a new line for legibility
 RD_EX	LEA	R0,	NWLN
 	TRAP	x22
-	BRNZP	HT
+
+	; check if counter is 7
+	ADD	R2,	R2,	#-7
+	; input holds something, proceed
+	BRN	FND
+	; else, tell the user theres nothing here and return to the start
+	LEA	R0,	ALTIN
+	TRAP	x22
+	BRNZP	START
 
 
-	; Finding Logic
-	LEA	R0,	PRMTFD
+; Find Input Logic
+FND	LEA	R0,	PRMTFD
 	TRAP	x22 ; PUTS
 
 	LD	R2,	COUNT2 ; load the number 2 into R2 for counting
 	LEA	R1,	FIND ; load pointer of Finding Input Storage
 
+	; init. R4 (this will be our counter for the next one)
+	AND	R4,	R4,	#0
+
 	; get character input without prompt
 FD_LP	TRAP	x20
-	; echo input
+	; echo character
 	TRAP	x21
 
+	; check if newline
+	AND	R3,	R3,	#0
+	ADD	R3,	R3,	R0
+	ADD	R3,	R3,	#-10 ; 10 is LF
+	BRZ	FD_EX ; if newline, continue
 
-	; Replacing Logic
+	; store the value
+	STR	R0,	R1,	#0
+
+	; increment pointer
+	ADD	R1,	R1,	#1
+
+	; inc counter for replace
+	ADD	R4,	R4,	#1
+	; decrement counter
+	ADD	R2,	R2,	#-1
+	BRP FD_LP
+
+FD_EX	LEA	R0,	NWLN
+	TRAP	x22
+
+	; check if counter is still two
+	ADD	R2,	R2,	#-2
+	; input holds something, proceed
+	BRN	RP
+
+	; else, tell the user theres nothing to find.
+	LEA	R0,	ALTFD
+	TRAP	x22
+	BRNZP	START
+
+; Replace In Logic
+
+RP	LEA	R0,	PRMTRP
+	TRAP	x22
+
+	LEA	R1,	REPLACE
+
+RP_LP	TRAP	x20
+	TRAP	x21
+
+	; check if newline
+	AND	R3,	R3,	#0
+	ADD	R3,	R3,	R0
+	ADD	R3,	R3,	#-10 ; 10 is LF
+	BRZ	RP_EX ; if newline, continue
+
+	; store val
+	STR	R0,	R1,	#0
+
+	; increment pointer
+	ADD	R1,	R1,	#1
+	; dec counter
+	ADD	R4,	R4,	#-1
+	; repeat if positive
+	BRP	RP_LP
+
+RP_EX	LEA	R0,	NWLN
+	TRAP	x22
+	BRNZP	START
 
 
-	; Printing Logic
+; replacement logic
+
+; printing logic
 
 
-	; Halting script
-
+; Halting script
 HT	TRAP x25
 
 
 ; var def
-INPUT	.BLKW		7
-FIND	.BLKW		2
-REPLACE	.BLKW		2
+INPUT	.BLKW		8 
+FIND	.BLKW		3
+REPLACE	.BLKW		3
 
 
-PRMTIN	.STRINGZ	"Input string of 7 or less characters: "
-PRMTFD	.STRINGZ	"Find: "
-PRMTRP	.STRINGZ	"Replace: "
+PRMTIN	.STRINGZ	"Input (max 7 chars): "
+PRMTFD	.STRINGZ	"Find (max 2 chars): "
+PRMTRP	.STRINGZ	"Replace (<= length of find): "
 
-NWLN	.FILL		x0D0A ; All combinations of CRLF just cause a square character, dunno why
+ALTIN	.STRINGZ	"Input empty, nothing to do."
+ALTFD	.STRINGZ	"Search query empty, nothing to do."
+
+
+NWLN	.FILL		x0D0A ; All combinations of CR and LF just cause a square character, dunno why
 
 COUNT7	.FILL		x0007
 COUNT2	.FILL		x0002
